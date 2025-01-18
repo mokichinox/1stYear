bce3b1a3d0e7f57ccdd5fc83cc77bc2928596700
Alessandra Veronica Braulio <yooa7551@gmail.com>
2025-01-19 00:07:15 +0800

    Create Main.java

---
+++ Comprog2/exer5/Main.java
@@ -0,0 +1,12 @@
+public class Main{
+ 
+ public static void main(String [] args) {
+     
+     Student student = new Student();
+     
+     student.setName("Maria");
+     student.setAge(19);
+     student.printStudent();
+ }
+    
+}
