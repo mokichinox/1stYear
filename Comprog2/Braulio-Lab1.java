779fc8279425e4534dbd5ff2e57c32cc21c956aa
Alessandra Veronica Braulio <yooa7551@gmail.com>
2025-01-19 00:00:33 +0800

    Create Braulio-Lab1.java

---
+++ Comprog2/Braulio-Lab1.java
@@ -0,0 +1,12 @@
+public class Main
+{
+	public static void main(String[] args) {
+	
+	String name = "Alessandra Veronica Braulio";
+	int age  = 19;
+	String hobby= "dance and read manga";
+	
+		System.out.println("Hi! My name is " + name + " and I am " + age + " years old. On my free time I like to " + hobby);
+	}
+}
+
